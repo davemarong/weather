298f47ab41693c77ce89c1378208e59094d10650
Dave Marong <kmarong@hotmail.com>
2021-03-20 06:27:47 +0100

    COMPLETE -- Weather Application without style

--- src/Apicall.js
+++ src/Apicall.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Apicall() {
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState({ name: "", main: { temp: "" } });
   const [location, setlocation] = useState("");
+
+  const handleInputChange = (event) => setlocation(event.target.value);
   const options = {
     method: "GET",
     url: "https://community-open-weather-map.p.rapidapi.com/weather",
     params: {
-      q: "Oslo",
+      q: location,
       lang: "Norwegian",
       units: "metric",
       mode: "xml, html",
@@ -18,19 +20,30 @@ export default function Apicall() {
       "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
     },
   };
-  React.useEffect(function effectFunction() {
+  const handleClick = () => {
     axios
       .request(options)
       .then((res) => {
         setWeather(res.data);
       })
-      .catch((fail) => console.log(fail));
-  }, []);
-  console.log(weather);
+      .catch((fail) =>
+        setWeather({
+          name: "Helt ærlig bror, det er ikke et sted ass.",
+          main: { temp: "" },
+        })
+      );
+  };
   return (
     <div>
       <h1>{weather.name}</h1>
       <h2>{weather.main.temp}</h2>
+      <input
+        onChange={handleInputChange}
+        type="text"
+        placeholder="Søk...
+      "
+      ></input>
+      <button onClick={handleClick}>Søk</button>
       <div></div>
     </div>
   );
